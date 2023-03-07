97bdd0370302ff0e671cd2e809797786f0345d6e
nls-cdragu <119405629+cdragu-n@users.noreply.github.com>
2023-03-07 15:31:06 +0200

    refactored

--- src/test/java/tests/UpdateUserTest.java
+++ src/test/java/tests/UpdateUserTest.java
@@ -29,53 +29,53 @@ public class UpdateUserTest {
 
     @Test
     public void putUserTest() {
-        User userUpdateValues = User.builder()
+        User user = User.builder()
                 .age(30)
                 .name(RandomStringUtils.randomAlphabetic(10))
                 .sex(Gender.FEMALE)
                 .zipCode(zipcode)
                 .build();
 
-        int statusCode = client.putUser(new UserToUpdate(userToChange, userUpdateValues));
+        int statusCode = client.putUser(new UserToUpdate(userToChange, user));
 
         ResponseEntity<List<User>> usersResponseAfterPut = client.getUsers();
         Assertions.assertAll("Asserting Put User test",
                 () -> Assertions.assertEquals(200, statusCode, "Status Code is not 200"),
-                () -> Assertions.assertTrue(usersResponseAfterPut.getBody().contains(userUpdateValues), "Response body does not contain user"),
+                () -> Assertions.assertTrue(usersResponseAfterPut.getBody().contains(user), "Response body does not contain user"),
                 () -> Assertions.assertFalse(usersResponseAfterPut.getBody().contains(userToChange), "Response body contains old user"));
     }
 
     @Test
     public void putUserWithUnavailableZipcodeTest() {
-        User userUpdateValues = User.builder()
+        User user = User.builder()
                 .age(30)
                 .name(RandomStringUtils.randomAlphabetic(10))
                 .sex(Gender.FEMALE)
                 .zipCode("UnavailableZipcode")
                 .build();
 
-        int statusCode = client.putUser(new UserToUpdate(userToChange, userUpdateValues));
+        int statusCode = client.putUser(new UserToUpdate(userToChange, user));
 
         ResponseEntity<List<User>> usersResponseAfterPut = client.getUsers();
         Assertions.assertAll("Asserting Put User with Unavailable Zipcode test",
                 () -> Assertions.assertEquals(424, statusCode, "Status Code is not 424"),
-                () -> Assertions.assertFalse(usersResponseAfterPut.getBody().contains(userUpdateValues), "Response body contains new user"),
+                () -> Assertions.assertFalse(usersResponseAfterPut.getBody().contains(user), "Response body contains new user"),
                 () -> Assertions.assertTrue(usersResponseAfterPut.getBody().contains(userToChange), "Response body does not contain old user"));
     }
 
     @Test
     public void putUserWithMissingRequiredFieldsTest() {
-        User userUpdateValues = User.builder()
+        User user = User.builder()
                 .age(30)
                 .zipCode(zipcode)
                 .build();
 
-        int statusCode = client.putUser(new UserToUpdate(userToChange, userUpdateValues));
+        int statusCode = client.putUser(new UserToUpdate(userToChange, user));
 
         ResponseEntity<List<User>> usersResponseAfterPut = client.getUsers();
         Assertions.assertAll("Asserting Put User with missing required fields test",
                 () -> Assertions.assertEquals(409, statusCode, "Status Code is not 409"),
-                () -> Assertions.assertFalse(usersResponseAfterPut.getBody().contains(userUpdateValues), "Response body contains new user"),
+                () -> Assertions.assertFalse(usersResponseAfterPut.getBody().contains(user), "Response body contains new user"),
                 () -> Assertions.assertTrue(usersResponseAfterPut.getBody().contains(userToChange), "Response body does not contain old user"));
     }
 }
